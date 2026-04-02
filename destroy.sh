6f57ca2161778c90e964c544607383aa9acb483d
Ajay Chankramath <your-github-email@example.com>
2026-04-01 21:18:39 -0600

    Remove unnecessary root-level files and directories

--- destroy.sh
+++
@@ -1,5 +0,0 @@
-# Variables for TLS in Terraform
-export TF_VAR_tls_cert_string=$TLS_CERT_STRING
-export TF_VAR_tls_key_string=$TLS_KEY_STRING
-
-terraform -chdir=setup/terraform destroy -auto-approve
